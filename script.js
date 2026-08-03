8e66e57aea29d4c2c5cbc7c5a51a39fca419c6f5
spandanareddy09-arch <spandanareddy0913@gmail.com>
2026-08-04 00:12:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
+// Hide loader
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        const loader = document.getElementById("loader");
+        loader.style.opacity = "0";
 
+        setTimeout(() => {
+            loader.style.display = "none";
+        }, 1000);
+
+    }, 3200);
+});
+
+
+// Floating Hearts
+
+const hearts = document.querySelector(".hearts");
+
+function createHeart(){
+
+    const heart=document.createElement("div");
+
+    heart.classList.add("heart");
+
+    heart.innerHTML="🤍";
+
+    heart.style.left=Math.random()*100+"vw";
+
+    heart.style.fontSize=(Math.random()*20+15)+"px";
+
+    heart.style.animationDuration=(Math.random()*4+6)+"s";
+
+    hearts.appendChild(heart);
+
+    setTimeout(()=>{
+        heart.remove();
+    },9000);
+
+}
+
+setInterval(createHeart,350);
